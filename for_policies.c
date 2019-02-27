83557fd676b031eb0fff5c5fcfa6028eb31a245c
Quentin Guilloteau <Quentin.Guilloteau@grenoble-inp.org>
2019-02-27 18:15:58 +0100

    Add comments to for_policies.c

--- for_policies.c
+++ for_policies.c
@@ -2,11 +2,23 @@
 #include <omp.h>
 #include "omp_logs.h"
 
+/* Goal: Log a sum in parallel
+ *
+ */
+
+/* As we need to sum to int,
+ * we need a struct that can hold to int
+ */
 struct data{
     int x;
     int* sum;
 };
 
+/* This is the function that we are going to log
+ * it must take a void* and return void
+ * So we pass it a void* that we will cast after in struct data
+ * And then we sum
+ */
 void sum(void* d) {
     struct data* data = (struct data*) d;
     *(data->sum) += data->x;
@@ -19,18 +31,29 @@ int main(int argc, char** argv) {
         return 1;
     }
     int N = atoi(argv[1]);
+
+    // We create the list of tasks
     struct task_list* l = NULL;
+
     int s = 0;
+    // A nice for in parallel with openMP
     #pragma omp parallel for schedule(static) reduction (+:s)
     for (int j = 0; j < N; j++) {
+        // We create the structure to hold the ints
         struct data d = {j, &s};
+        /* We log the task
+         * We give it the info j which is the number that it is adding
+         */
         log_task(&l, "Sum", j, omp_get_thread_num(), sum, (void*) &d);
     }
+    // We save the svg
     tasks_to_svg(l, "for_static.svg");
+    // And we free the list of tasks
     free_list(l);
     l = NULL;
 
 
+    s = 0;
     #pragma omp parallel for schedule(dynamic) reduction (+:s)
     for (int j = 0; j < N; j++) {
         struct data d = {j, &s};
@@ -41,6 +64,7 @@ int main(int argc, char** argv) {
     free_list(l);
     l = NULL;
 
+    s = 0;
     #pragma omp parallel for schedule(guided) reduction (+:s)
     for (int j = 0; j < N; j++) {
         struct data d = {j, &s};
